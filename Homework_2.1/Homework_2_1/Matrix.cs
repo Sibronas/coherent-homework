b41d46ed8fc0c32cca0d246b26591ae07b9bfc8c
Sibronas <norb.jankauskas@gmail.com>
2022-01-13 22:09:47 +0200

    Matrix homework done commit

---
+++ Homework_2.1/Homework_2_1/Matrix.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_1
+{
+    public class Matrix
+    {
+        public int[] Diagonal { get; set; }
+        public int Size { get; set; }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i != j)
+                {
+                    return 0;
+                }
+
+
+                if (i < 0 || i >= Size || j < 0 || j >= Size)
+                {
+                    return 0;
+                }
+
+
+                return Diagonal[i];
+            }
+
+            set
+            {
+                if (i != j)
+                {
+                    return;
+                }
+
+                if (i < 0 || i >= Size || j < 0 || j >= Size)
+                {
+                    return;
+                }
+
+
+                Diagonal[i] = value;
+            }
+        }
+
+        public Matrix(params int[] members)
+        {
+            Diagonal = members ?? Array.Empty<int>();
+            Size = Diagonal.Length;
+        }
+
+        public int Track() => Diagonal.Sum();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null or not Matrix)
+            {
+                return false;
+            }
+
+            var matrixToCompare = (Matrix)obj;
+
+            if (Size != matrixToCompare.Size)
+            {
+                return false;
+            }
+
+            return Diagonal.SequenceEqual(matrixToCompare.Diagonal);
+        }
+
+        public override string ToString()
+        {
+            return "Matrix class ToString is overriden!";
+        }
+    }
+}
---
+++ Homework_2.1/Homework_2_1/MatrixExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_1
+{
+    public static class MatrixExtensions
+    {
+        public static Matrix Add(this Matrix firstMatrix, Matrix secondMatrix)
+        {
+            var diagonal = new List<int>();
+
+            if (firstMatrix.Size == secondMatrix.Size)
+            {
+                for (var i = 0; i < firstMatrix.Size; i++)
+                {
+                    diagonal.Add(firstMatrix[i, i] + secondMatrix[i, i]);
+                }
+
+                return new Matrix(diagonal.ToArray());
+            }
+            else if (firstMatrix.Size > secondMatrix.Size)
+            {
+                var secondMatrixWithPadding = new List<int>();
+
+                for (var i = 0; i < firstMatrix.Size; i++)
+                {
+                    if (i > secondMatrix.Size)
+                    {
+                        secondMatrixWithPadding.Add(0);
+                    }
+
+                    secondMatrixWithPadding.Add(secondMatrix[i, i]);
+                }
+
+                for (var i = 0; i < firstMatrix.Size; i++)
+                {
+                    diagonal.Add(firstMatrix[i, i] + secondMatrixWithPadding[i]);
+                }
+            }
+            else
+            {
+                var firstMatrixWithPadding = new List<int>();
+
+                for (var i = 0; i < secondMatrix.Size; i++)
+                {
+                    if (i > firstMatrix.Size)
+                    {
+                        firstMatrixWithPadding.Add(0);
+                    }
+
+                    firstMatrixWithPadding.Add(firstMatrix[i, i]);
+                }
+
+                for (var i = 0; i < secondMatrix.Size; i++)
+                {
+                    diagonal.Add(firstMatrixWithPadding[i] + secondMatrix[i, i]);
+                }
+            }
+
+            return new Matrix(diagonal.ToArray());
+        }
+    }
+}
